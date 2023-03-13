cff76a898df4b2a9d4a150f9731cba4f6f5edbcd
Mariaericka <98100913+Mariaericka@users.noreply.github.com>
2023-03-13 16:24:34 +0800

    Update user_header.php
    pinalakihan ang logo

--- components/user_header.php
+++ components/user_header.php
@@ -15,7 +15,10 @@ if(isset($message)){
 
    <section class="flex">
 
-   <a href="../index.php" class="logo"><img src="images/omg-logo.png" image style="object-fit: contain; width: 70px;"></a>
+   <a href="../index.php" class="logo"><img src="images/omg-logo.png" image style="    position: absolute;
+    width: 117px;
+    height: 115px;
+    top: 0px;"></a>
 
       <nav class="navbar">
          <a href="index.php">HOME</a>
