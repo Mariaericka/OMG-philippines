31efd24cb7a89458ca12c76bebbfb1bbefe83315
Mariaericka <98100913+Mariaericka@users.noreply.github.com>
2023-04-03 22:24:07 +0800

    Update menu.php

--- menu.php
+++ menu.php
@@ -26,7 +26,7 @@ include 'components/add_cart.php';
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
    <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/styles.css">
+   <link rel="stylesheet" href="css/stylez.css">
 
 </head>
 <body>
