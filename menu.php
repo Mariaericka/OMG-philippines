777496e36d40a934899226d8927c923e86456a56
AnthonyCasaul <anthonycasaul17@gmail.com>
2023-06-18 23:16:54 +0800

    display the categories and products

--- menu.php
+++ menu.php
@@ -11,7 +11,8 @@ if(isset($_SESSION['user_id'])){
 };
 
 include 'components/add_cart.php';
-
+$select_categories = $conn->prepare("SELECT * FROM `omg_categories`");
+$select_categories->execute();
 ?>
 
 <!DOCTYPE html>
@@ -24,6 +25,7 @@ include 'components/add_cart.php';
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
 
    <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style2.css">
@@ -39,23 +41,50 @@ include 'components/add_cart.php';
 <!-- menu section starts  -->
 <div class="second"><ul>
     <h1>Categories</h1> <br>
-    <li> <a href="menu.php" class="button14">Back To Main Menu</a></li>
-        <li> <a href="category.php?category=coffee series" class="button14">COFFEE SERIES</a></li>
-        <li> <a href="category.php?category=yogurt"  class="button14">YOGURT SERIES</a></li>
-        <li> <a href="category.php?category=choco" class="button14">CHOCO SERIES</a></li>
-        <li><a href="category.php?category=milktea" class="button14">MILKTEA SERIES</a></li>
-        <li><a href="category.php?category=mango" class="button14">MANGO SERIES</a></li>
-      </ul>
+    <div class="flex-container">
+    <?php
+    // Fetch the results using a while loop
+    while ($row = $select_categories->fetch(PDO::FETCH_ASSOC)) {
+        $category_name = $row['category_name'];
+        $category_image = $row['category_img'];
+        $category_id = $row['category_id'];
+        echo '<div><img src="images/category/sample.jpg" height="100px" width="100px" onclick="selectCategory('.$category_id.')"/><br>'.$category_name.'</div>';
+    }
+    ?>
     </div>
+    </div>
+<div>
+
+
+
+
+
+
+
+
+</div>
 
 <div class="loader">
    <img src="images/loading.gif" alt="">
 </div>
 <!-- footer section starts  -->
-<?php include 'components/footer.php'; ?>
+<!-- <?php include 'components/footer.php'; ?> -->
 <!-- footer section ends -->
 <!-- custom js file link  -->
 <script src="js/script.js"></script>
 
 </body>
 </html>
+<script>
+function selectCategory(id) {
+            console.log(id);
+            $.ajax({
+         type: "POST",
+         url: "product_list.php",
+         data: {"id": id},
+         success: function(result){
+            console.log(result);
+         }
+      });	
+   }
+</script>
