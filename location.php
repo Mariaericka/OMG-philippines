70caeea5ee8468994d6c2b132ca2b15c349fa870
Mariaericka <MARIAERICKA.TOLEDO@live.ama.edu.ph>
2023-07-12 23:57:58 +0800

    lcoation  page media screen

--- location.php
+++ location.php
@@ -56,7 +56,7 @@ if(isset($_SESSION['user_id'])){
         
         </ul>
         <div class="column">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d123857.6625681424!2d121.17941868960693!3d14.044552059086897!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd68306a6d54d1%3A0xef3ccbe688f795d0!2sAlaminos%2C%20Laguna!5e0!3m2!1sen!2sph!4v1648655970731!5m2!1sen!2sph" width="600" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d123857.6625681424!2d121.17941868960693!3d14.044552059086897!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd68306a6d54d1%3A0xef3ccbe688f795d0!2sAlaminos%2C%20Laguna!5e0!3m2!1sen!2sph!4v1648655970731!5m2!1sen!2sph" class="map" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
           
     </div>
@@ -70,7 +70,7 @@ if(isset($_SESSION['user_id'])){
          <h3>Kalye Ekis Batong Malake Los Banos ,Laguna</h3>
     </ul>
         <div class="column">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3868.271867785642!2d121.223805714316!3d14.17884839107516!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd61c26b0ccb55%3A0x13b1aac46416a0cb!2sKalye%20Ekis!5e0!3m2!1sen!2sph!4v1648653645767!5m2!1sen!2sph" width="600" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3868.271867785642!2d121.223805714316!3d14.17884839107516!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd61c26b0ccb55%3A0x13b1aac46416a0cb!2sKalye%20Ekis!5e0!3m2!1sen!2sph!4v1648653645767!5m2!1sen!2sph" class="map" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
         </div>
     </div>
@@ -83,7 +83,7 @@ if(isset($_SESSION['user_id'])){
          <h3>Purok 6 Bulanao Tabuk City, Kalinga</h3>
         </ul>
         <div class="column">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d243643.7478208925!2d121.27383625607227!3d17.41497585935995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x338f99c3d1c5399b%3A0xc919e747c62ce9f4!2sTabuk%2C%20Kalinga!5e0!3m2!1sen!2sph!4v1648654542995!5m2!1sen!2sph" width="600" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d243643.7478208925!2d121.27383625607227!3d17.41497585935995!2m3!1f0!2f0!3f0!3m2!1i1024! 2i768!4f13.1!3m3!1m2!1s0x338f99c3d1c5399b%3A0xc919e747c62ce9f4!2sTabuk%2C%20Kalinga!5e0!3m2!1sen!2sph!4v1648654542995!5m2!1sen!2sph" class="map" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             
             </div></div>
             </section>
@@ -94,7 +94,7 @@ if(isset($_SESSION['user_id'])){
          <h3>Rizal Avenue Brgy.VII-D San Pablo,Laguna</h3>
         </ul>
         <div class="column">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3870.1008202918697!2d121.32077421431478!3d14.07122339360341!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd5cce828a872d%3A0x6395d9e9037bfd6c!2sJose%20Rizal%20Ave%2C%20San%20Pablo%20City%2C%20Laguna!5e0!3m2!1sen!2sph!4v1648654951759!5m2!1sen!2sph" width="600" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3870.1008202918697!2d121.32077421431478!3d14.07122339360341!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd5cce828a872d%3A0x6395d9e9037bfd6c!2sJose%20Rizal%20Ave%2C%20San%20Pablo%20City%2C%20Laguna!5e0!3m2!1sen!2sph!4v1648654951759!5m2!1sen!2sph" class="map" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             
             </div>
 </div>
@@ -107,7 +107,7 @@ if(isset($_SESSION['user_id'])){
          <h3>8 Governor A. Carpio Ave, Brgy San Pedro. Sto. Tomas City, Batangas</h3>
         </ul>
         <div class="column">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15479.431960389144!2d121.16956461969174!3d14.085559154744246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd6601e5f94371%3A0x6ef189aab0e81333!2sSan%20Pedro%2C%20Santo%20Tomas%2C%20Batangas!5e0!3m2!1sen!2sph!4v1648655318698!5m2!1sen!2sph" width="600" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15479.431960389144!2d121.16956461969174!3d14.085559154744246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd6601e5f94371%3A0x6ef189aab0e81333!2sSan%20Pedro%2C%20Santo%20Tomas%2C%20Batangas!5e0!3m2!1sen!2sph!4v1648655318698!5m2!1sen!2sph" class="map" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             
             </div></div></section>
             <div class="loader">
