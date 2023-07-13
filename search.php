b1ce3711b26e45eb9a5bec51aac74a44719e99aa
Mariaericka <MARIAERICKA.TOLEDO@live.ama.edu.ph>
2023-07-13 17:12:27 +0800

    added modal in products search

--- search.php
+++ search.php
@@ -27,6 +27,8 @@ include 'components/add_cart.php';
 
    <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style2.css">
+   <link rel="stylesheet" href="css/modal.css">
+
 
 </head>
 <body>
@@ -65,27 +67,20 @@ include 'components/add_cart.php';
          <input type="hidden" name="price" value="<?= $fetch_products['price']; ?>">
          <input type="hidden" name="priceR" value="<?= $fetch_products['description']; ?>">
 
-    <input type="hidden" name="pid" value="<?= $fetch_products['id']; ?>">
-         <input type="hidden" name="name" value="<?= $fetch_products['name']; ?>">
-         <input type="hidden" name="price" value="<?= $fetch_products['price']; ?>">
-         <input type="hidden" name="priceR" value="<?= $fetch_products['description']; ?>">
-
 
          <input type="hidden" name="image" value="<?= $fetch_products['image']; ?>">
          <div class="omg-menu-img">
 
-         <img src="images/<?= $fetch_products['image']; ?>" alt="" class="img1"></div>
-         <a href="category.php?category=<?= $fetch_products['category']; ?>" class="cat"><?= $fetch_products['category']; ?></a>
-         <h4><?= $fetch_products['name']; ?></h4>
-         
-            <div class="cat"><span>₱</span><?= $fetch_products['price']; ?></div>
-           Qty <input type="number" name="qty" class="qty" min="1" max="99" value="1" maxlength="2">
+         <img src="images/<?= $fetch_products['image']; ?>" alt="" class="img1" onclick="openModal()"></div>
+         <div class="cat"><?= $fetch_products['name']; ?></div>
+         <h4 style="font-size: initial; background-color: #FFD93D;"> Starts at <span>₱</span><?= $fetch_products['price']; ?>.00</h4>
+           <!-- Qty <input type="number" name="qty" class="qty" min="1" max="99" value="1" maxlength="2"> -->
             <p class="omg-detail">
                             <div class="omg-menu-desc">
                             <?= $fetch_products['description']; ?>
                                     </p></div>
                  
-        <button type="submit" name="add_to_cart" class="btn">ADD TO CART</button>
+        <!-- <button type="submit" name="add_to_cart" class="btn">ADD TO CART</button> -->
 
          
       </form>
@@ -105,6 +100,44 @@ include 'components/add_cart.php';
 
 
 
+<!-- The Modal -->
+<div id="costumizeOrderModal" class="backdrop">
+
+  <!-- Modal content -->
+  <div class="modal">
+      <div class="modal-header">
+         <span class="close" onclick="closeModal()">&times;</span>
+      </div>
+      <div class="modal-body">
+         <table>
+            <tr>
+               <td>
+                  <span class="modal-label">Size:</span>
+               </td>
+               <td>
+                  <select class="input" id="size-dropdown">
+                     <option value="small" selected>Regular</option>
+                     <option value="large">Large</option>
+                  </select>
+               </td>
+               <tr>
+               <td>
+                  <span class="modal-label">Quantity:</span>
+               </td>
+               <td>
+           <input type="number" name="qty" class="qty" min="1" max="99" value="1" maxlength="2">
+           </td>
+            </tr>
+         </table>
+      </div>
+      <div class="modal-footer">
+         <button class="btn confirm-btn" name="add_to_cart">ADD TO CART</button>
+         <button class="btn close-btn" onclick="closeModal()">CANCEL</button>
+      </div>
+  </div>
+
+
+</div>
 
 
 
@@ -123,6 +156,7 @@ include 'components/add_cart.php';
 
 <!-- custom js file link  -->
 <script src="js/script.js"></script>
+<script src="js/modal.js"></script>
 
 </body>
 </html>
