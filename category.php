6d1673cd32b91df5790b652faf0a14df980019a1
Mariaericka <MARIAERICKA.TOLEDO@live.ama.edu.ph>
2023-07-18 14:58:04 +0800

    for the menu

--- category.php
+++ category.php
@@ -1,17 +1,15 @@
 <?php
-
 include 'components/connect.php';
 
 session_start();
 
-if(isset($_SESSION['user_id'])){
+if (isset($_SESSION['user_id'])) {
    $user_id = $_SESSION['user_id'];
-}else{
+} else {
    $user_id = '';
-};
+}
 
 include 'components/add_cart.php';
-
 ?>
 
 <!DOCTYPE html>
@@ -33,155 +31,91 @@ include 'components/add_cart.php';
 <body>
    
 <?php include 'components/user_header.php'; ?>
-<div class="second"><ul>
-    <br> <h1>Categories</h1>
-    <li> <a href="menu.php" class="button14"><i class="fa fa-long-arrow-left" style=""></i> Go Back</a></li>
+<div class="second">
+   <ul>
+      <br>
+      <h1>Categories</h1>
+      <li><a href="menu.php" class="button14"><i class="fa fa-long-arrow-left" style=""></i> Go Back</a></li>
+      <li><a href="category.php?category=coffee series" class="button14">COFFEE SERIES<i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>
+      <li><a href="category.php?category=yogurt" class="button14">YOGURT SERIES<i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>
+      <li><a href="category.php?category=choco" class="button14">CHOCO SERIES<i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>
+      <li><a href="category.php?category=milktea" class="button14">MILKTEA SERIES<i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>
+      <li><a href="category.php?category=mango" class="button14">MANGO SERIES<i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>
+   </ul>
+</div>
 
-        <li> <a href="category.php?category=coffee series" class="button14">COFFEE SERIES<i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>
-        <li> <a href="category.php?category=yogurt"  class="button14">YOGURT SERIES<i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>
-        <li> <a href="category.php?category=choco" class="button14">CHOCO SERIES<i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>
-        <li><a href="category.php?category=milktea" class="button14">MILKTEA SERIES<i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>
-        <li><a href="category.php?category=mango" class="button14">MANGO SERIES<i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>
-      </ul>
-    </div>
-    <section>
-    <div class="container2" style="box-sizing: border-box;
-height: 800px;
-margin-left: 25%;">
-
-
-
-
-<section class="products">
-
-  
-
-   <div class="box-container">
-
-      <?php
-         $category = $_GET['category'];
-         $select_products = $conn->prepare("SELECT * FROM `products` WHERE category = ?");
-         $select_products->execute([$category]);
-         if($select_products->rowCount() > 0){
-            while($fetch_products = $select_products->fetch(PDO::FETCH_ASSOC)){
-      ?>
-     <form action="" method="post" class="box">
-         <input type="hidden" name="pid" value="<?= $fetch_products['id']; ?>">
-         <input type="hidden" name="name" value="<?= $fetch_products['name']; ?>">
-         <input type="hidden" name="price" value="<?= $fetch_products['price']; ?>">
-         <input type="hidden" name="priceR" value="<?= $fetch_products['description']; ?>">
-
-
-         <input type="hidden" name="image" value="<?= $fetch_products['image']; ?>">
-         <div class="omg-menu-img">
-
-         <img src="images/<?= $fetch_products['image']; ?>" alt="" class="img1" onclick="openModal()"></div>
-         <div class="cat"><?= $fetch_products['name']; ?></div>
-         <h4 style="font-size: initial; background-color: #FFD93D;"> Starts at <span>₱</span><?= $fetch_products['price']; ?>.00</h4>
-         <h4 style="font-size: initial; background-color: #FFD93D;"> Starts at <span>₱</span><?= $fetch_products['priceR']; ?>.00</h4>
-
-           <!-- Qty <input type="number" name="qty" class="qty" min="1" max="99" value="1" maxlength="2"> -->
-            <p class="omg-detail">
-                            <div class="omg-menu-desc">
-                            <?= $fetch_products['description']; ?>
-                                    </p></div>
-                 
-        <!-- <button type="submit" name="add_to_cart" class="btn">ADD TO CART</button>-->
-
-         
-      </form>
-      <?php
+<section>
+   <div class="container2" style="box-sizing: border-box; height: 800px; margin-left: 25%;">
+      <section class="products">
+         <div class="box-container">
+            <?php
+            $category = $_GET['category'];
+            $select_products = $conn->prepare("SELECT * FROM `products` WHERE category = ?");
+            $select_products->execute([$category]);
+            if ($select_products->rowCount() > 0) {
+               while ($fetch_products = $select_products->fetch(PDO::FETCH_ASSOC)) {
+            ?>
+                  <div class="box">
+                     <div class="omg-menu-img">
+                        <img src="images/<?= $fetch_products['image']; ?>" alt="" class="img1" onclick="openModal('<?= $fetch_products['id']; ?>', '<?= $fetch_products['name']; ?>', '<?= $fetch_products['price']; ?>', '<?= $fetch_products['priceR']; ?>', '<?= $fetch_products['image']; ?>')">
+                     </div>
+                     <div class="cat"><?= $fetch_products['name']; ?></div>
+                     <h4 style="font-size: initial; background-color: #FFD93D;"> Starts at <span>₱</span><?= $fetch_products['price']; ?>.00</h4>
+                     <h4 style="font-size: initial; background-color: #FFD93D;"> Starts at <span>₱</span><?= $fetch_products['priceR']; ?>.00</h4>
+                     <div class="omg-menu-desc">
+                        <?= $fetch_products['description']; ?>
+                     </div>
+                  </div>
+                  <div id="costumizeOrderModal<?= $fetch_products['id']; ?>" class="backdrop">
+                     <div class="modal">
+                        <div class="modal-header">
+                           <span class="close" onclick="closeModal(<?= $fetch_products['id']; ?>)">&times;</span>
+                        </div>
+                        <div class="modal-body">
+                           <table>
+                              <tr>
+                                 <td>
+                                    <span class="modal-label">Size:</span>
+                                 </td>
+                                 <td>
+                                    <select class="input" id="size-dropdown<?= $fetch_products['id']; ?>">
+                                       <option value="small" selected>Regular <span>₱</span><span id="modal-regular-price<?= $fetch_products['id']; ?>"></span><?= $fetch_products['price']; ?>.00</option>
+                                       <option value="large">Large <span>₱</span><span id="modal-large-price<?= $fetch_products['id']; ?>"></span><?= $fetch_products['priceR']; ?>.00</option>
+                                    </select>
+                                 </td>
+                              </tr>
+                              <tr>
+                                 <td>
+                                    <span class="modal-label">Quantity:</span>
+                                 </td>
+                                 <td>
+                                    <input type="number" name="qty" class="qty" min="1" max="99" value="1" maxlength="2">
+                                 </td>
+                              </tr>
+                           </table>
+                        </div>
+                        <div class="modal-footer">
+                           <button class="btn confirm-btn" onclick="addToCart(<?= $fetch_products['id']; ?>)">ADD TO CART</button>
+                           <button class="btn close-btn" onclick="closeModal(<?= $fetch_products['id']; ?>)">CANCEL</button>
+                        </div>
+                     </div>
+                  </div>
+            <?php
+               }
+            } else {
+               echo '<p class="empty">No drinks added yet!</p>';
             }
-         }else{
-            echo '<p class="empty">No drinks added yet!</p>';
-         }
-      ?>
-
-   </div>
+            ?>
+         </div>
+      </section>
    </div>
 </section>
 
-
-    
-
-
-
-     
-
-<?php
-         $category = $_GET['category'];
-         $select_products = $conn->prepare("SELECT * FROM `products` WHERE category = ?");
-         $select_products->execute([$category]);
-     
-      ?>
-
-
-
-
-<!-- The Modal -->
-<div id="costumizeOrderModal" class="backdrop">
-<?php
-    if ($select_products->rowCount() > 0) {
-        while ($fetch_products = $select_products->fetch(PDO::FETCH_ASSOC)) {
-            ?>
-  <!-- Modal content -->
-  <div class="modal">
- 
-      <div class="modal-header">
-         <span class="close" onclick="closeModal()">&times;</span>
-      </div>
-      <div class="modal-body">
-      
-         <table>
-      
-            <tr>
-               <td>
-                  <span class="modal-label">Size:</span>
-               </td>
-               <td>
-                  <select class="input" id="size-dropdown">
-                     <option value="small" selected>Regular <span>₱</span><?= $fetch_products['price']; ?>.00 </option>
-                     <option value="large">Large <span>₱</span><?= $fetch_products['priceR']; ?>.00</option>
-                  </select>
-               </td>
-               <tr>
-               <td>
-                  <span class="modal-label">Quantity:</span>
-               </td>
-               <td>
-           <input type="number" name="qty" class="qty" min="1" max="99" value="1" maxlength="2">
-           </td>
-            </tr>
-       
-         </table>
-      
-      </div>
-      <div class="modal-footer">
-         <button class="btn confirm-btn" name="add_to_cart">ADD TO CART</button>
-         <button class="btn close-btn" onclick="closeModal()">CANCEL</button>
-       
-      </div>
-    
-  </div>
-  <?php
-            }
-         }else{
-            echo '<p class="empty">No drinks added yet!</p>';
-         }
-      ?>
-
-
-
-
-</div>
-
 <?php include 'components/footer.php'; ?>
 
-
 <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
-
-<!-- custom js file link  -->
 <script src="js/script.js"></script>
 <script src="js/modal.js"></script>
+
 </body>
 </html>
--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,64 @@
 // Get the modal
 var modal = document.getElementById("costumizeOrderModal");
 
-function openModal(){
-    modal.style.display = "block";
+function openModal(productId) {
+  var sizeDropdown = document.getElementById("size-dropdown" + productId);
+  var regularPriceLabel = document.getElementById("modal-regular-price" + productId);
+  var largePriceLabel = document.getElementById("modal-large-price" + productId);
+
+  // Get the prices using AJAX
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var response = JSON.parse(xhr.responseText);
+      regularPriceLabel.textContent = response.regularPrice;
+      largePriceLabel.textContent = response.largePrice;
+    }
+  };
+  xhr.open("GET", "get_prices.php?productId=" + productId, true);
+  xhr.send();
+
+  modal.style.display = "block";
 }
-function closeModal(){
-    modal.style.display = "none";
+
+function closeModal() {
+  modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+window.addEventListener("click", function(event) {
+  if (event.target === modal) {
+    closeModal();
   }
+});
+
+function addToCart(productId) {
+  // Get the selected size and quantity from the modal
+  var sizeDropdown = document.getElementById("size-dropdown" + productId);
+  var selectedSize = sizeDropdown.value;
+  var quantityInput = document.querySelector("#costumizeOrderModal .modal-body .qty");
+  var quantity = quantityInput.value;
+
+  // Perform any additional validation or processing if required
+
+  // Create an object or form data to send the selected product details to the server
+  var data = {
+    productId: productId,
+    size: selectedSize,
+    quantity: quantity
+  };
+
+  // Make an AJAX request to the server to add the product to the cart
+  // You can use libraries like Axios, jQuery, or the native fetch API for the AJAX request
+
+  // Example using Axios library:
+  axios.post("add_to_cart.php", data)
+    .then(function(response) {
+      // Handle the response from the server, such as displaying a success message or updating the cart view
+      console.log(response.data);
+    })
+    .catch(function(error) {
+      // Handle any errors that occur during the AJAX request
+      console.error(error);
+    });
 }
