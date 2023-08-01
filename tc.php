6bd2c914dbb75d9ceff5654ba276695b3c0c6506
Mariaericka <MARIAERICKA.TOLEDO@live.ama.edu.ph>
2023-08-01 17:10:24 +0800

    added privacy policy

--- tc.php
+++ tc.php
@@ -17,7 +17,7 @@ if(isset($_SESSION['user_id'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>OMG Philippines | Location</title>
+   <title>OMG Philippines | Terms and Condition</title>
    <link rel="icon"  href="images/omg-logo.png">
 
   <!-- font awesome cdn link  -->
