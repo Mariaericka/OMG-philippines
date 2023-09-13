bde22d6be1319badd43b2a0267edff35e1818995
Mariaericka <MARIAERICKA.TOLEDO@live.ama.edu.ph>
2023-09-13 17:21:34 +0800

    Create success.php

---
+++ success.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="shortcut icon" href="https://assets.xendit.co/checkout-staging/favicon.ico" />
+    <title>Checkout</title>
+    <!-- Google Tag Manager -->
+    <script>
+      (function (w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+        var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s),
+          dl = l != "dataLayer" ? "&l=" + l : "";
+        j.async = true;
+        j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+        f.parentNode.insertBefore(j, f);
+      })(
+        window,
+        document,
+        "script",
+        "dataLayer",
+        "GTM-WN8X7BC"
+      );
+    </script>
+    <!-- End Google Tag Manager -->
+    <script
+      type="text/javascript"
+      src="https://js.xendit.co/v1/xendit.min.js"
+    ></script>
+    <script type="module" crossorigin src="https://assets.xendit.co/checkout-staging/assets/index-8920e8aa.js"></script>
+    <link rel="stylesheet" href="https://assets.xendit.co/checkout-staging/assets/index-d60d4c9e.css">
+  </head>
+  <body class="antialiased">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+      <iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-WN8X7BC"
+        height="0"
+        width="0"
+        style="display: none; visibility: hidden"
+      ></iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+    <div id="root"><!--app-html--></div>
+
+    <main class="loading-container min-h-screen flex items-stretch">
+      <div class="flex-1 lg:px-8 xl:px-8 lg:py-8">
+        <div class="w-full mx-auto lg:max-w-2xl">
+          <div class="animate-pulse flex space-x-4">
+            <div class="flex-1 space-y-8 py-1">
+              <div class="h-16 bg-xen-gray-200 rounded"></div>
+
+              <div class="space-y-4 flex flex-col items-center">
+                <div class="h-5 w-80 bg-xen-gray-200 rounded"></div>
+                <div class="h-8 w-64 bg-xen-gray-200 rounded"></div>
+              </div>
+
+              <div class="pt-4">
+                <div class="h-4 w-32 bg-xen-gray-200 rounded"></div>
+
+                <div class="flex-1 pt-4 space-y-2">
+                  <div class="h-14 bg-xen-gray-200 rounded"></div>
+                  <div class="h-14 bg-xen-gray-200 rounded"></div>
+                  <div class="h-14 bg-xen-gray-200 rounded"></div>
+                  <div class="h-14 bg-xen-gray-200 rounded"></div>
+                </div>
+              </div>
+
+              <div class="flex justify-center pt-4">
+                <div class="w-32 h-8 bg-xen-gray-200 rounded"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <aside
+        class="hidden lg:flex flex-col w-2/5 xl:w-1/3 bg-xen-gray-100 px-4 xl:px-8 py-8"
+      >
+        <div class="w-full mx-auto max-w-sm">
+          <div class="animate-pulse flex space-x-4">
+            <div class="flex-1 space-y-8 pt-4">
+              <div class="space-y-2">
+                <div class="h-8 w-48 bg-xen-gray-200 rounded"></div>
+                <div class="h-4 bg-xen-gray-200 rounded"></div>
+              </div>
+
+              <div class="space-y-2">
+                <div class="h-5 bg-xen-gray-200 rounded"></div>
+                <div class="h-5 bg-xen-gray-200 rounded"></div>
+              </div>
+
+              <div class="flex flex-row space-x-2">
+                <div class="flex-1 space-y-2">
+                  <div class="h-7 bg-xen-gray-200 rounded"></div>
+                  <div class="h-4 bg-xen-gray-200 rounded"></div>
+                </div>
+                <div class="flex flex-1 justify-end">
+                  <div class="w-24 h-7 bg-xen-gray-200 rounded"></div>
+                </div>
+              </div>
+
+              <div class="flex flex-row space-x-2">
+                <div class="flex-1 space-y-2">
+                  <div class="h-7 bg-xen-gray-200 rounded"></div>
+                  <div class="h-4 bg-xen-gray-200 rounded"></div>
+                </div>
+                <div class="flex flex-1 justify-end">
+                  <div class="w-24 h-7 bg-xen-gray-200 rounded"></div>
+                </div>
+              </div>
+
+              <div class="pt-12 flex flex-row space-x-2">
+                <div class="flex-1 space-y-2">
+                  <div class="h-7 bg-xen-gray-200 rounded"></div>
+                </div>
+                <div class="flex flex-1 justify-end">
+                  <div class="w-24 h-7 bg-xen-gray-200 rounded"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </aside>
+    </main>
+    
+  </body>
+</html>
